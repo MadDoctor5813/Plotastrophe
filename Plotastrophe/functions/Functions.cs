f5490f3ccc1ad1ded063e83776468bcdcf3b82f1
Kevin <dragn194@gmail.com>
2018-01-12 21:35:14 -0500

    Implement a few more functions

--- Plotastrophe/functions/Functions.cs
+++ Plotastrophe/functions/Functions.cs
@@ -43,9 +43,9 @@ namespace Plotastrophe.functions
         }
     }
 
-    class RationalFunction : PlotFunction
+    class Reciprocal : PlotFunction
     {
-        public RationalFunction(PlotCanvas canvas) : base(canvas)
+        public Reciprocal(PlotCanvas canvas) : base(canvas)
         {
         }
 
@@ -54,9 +54,219 @@ namespace Plotastrophe.functions
             return 1 / x;
         }
 
-        protected override double[] Asymptotes()
+        protected override List<double> Asymptotes()
         {
-            return new double[1] { -D };
+            return new List<double> { -D };
+        }
+    }
+
+    class ExponentialFunction : PlotFunction
+    {
+        public ExponentialFunction(PlotCanvas canvas) : base(canvas)
+        {
+        }
+
+        protected override double Parent(double x)
+        {
+            return Math.Pow(2, x);
+        }
+    }
+
+    class LogarithmicFunction : PlotFunction
+    {
+        public LogarithmicFunction(PlotCanvas canvas) : base(canvas)
+        {
+        }
+
+        protected override double Parent(double x)
+        {
+            return Math.Log10(x);
+        }
+
+        protected override List<double> Asymptotes()
+        {
+            return new List<double> { -D };
+        }
+    }
+
+    class SinFunction : PlotFunction
+    {
+        public SinFunction(PlotCanvas canvas) : base(canvas)
+        {
+        }
+
+        protected override double Parent(double x)
+        {
+            return Math.Sin(x);
+        }
+    }
+
+    class CosFunction : PlotFunction
+    {
+        public CosFunction(PlotCanvas canvas) : base(canvas)
+        {
+        }
+
+        protected override double Parent(double x)
+        {
+            return Math.Cos(x);
+        }
+    }
+
+    class TanFunction : PlotFunction
+    {
+        public TanFunction(PlotCanvas canvas) : base(canvas)
+        {
+        }
+
+        protected override double Parent(double x)
+        {
+            return Math.Tan(x);
+        }
+
+        protected override List<double> Asymptotes()
+        {
+            List<double> asymptotes = new List<double>();
+            for (double i = Math.PI / 2; i < End; i += Math.PI)
+            {
+                asymptotes.Add(((1 / K) * i) - D);
+            }
+            for (double i = -Math.PI / 2; i > Start; i -= Math.PI)
+            {
+                asymptotes.Add(((1 / K) * i) - D);
+            }
+            return asymptotes;
+        }
+    }
+
+    class SecFunction : PlotFunction
+    {
+        public SecFunction(PlotCanvas canvas) : base(canvas)
+        {
+        }
+
+        protected override double Parent(double x)
+        {
+            return 1 / Math.Cos(x);
+        }
+
+        protected override List<double> Asymptotes()
+        {
+            List<double> asymptotes = new List<double>();
+            for (double i = Math.PI / 2; i < End; i += Math.PI)
+            {
+                asymptotes.Add(((1 / K) * i) - D);
+            }
+            for (double i = -Math.PI / 2; i > Start; i -= Math.PI)
+            {
+                asymptotes.Add(((1 / K) * i) - D);
+            }
+            return asymptotes;
+        }
+    }
+
+    class CscFunction : PlotFunction
+    {
+        public CscFunction(PlotCanvas canvas) : base(canvas)
+        {
+        }
+
+        protected override double Parent(double x)
+        {
+            return 1 / Math.Sin(x);
+        }
+
+        protected override List<double> Asymptotes()
+        {
+            List<double> asymptotes = new List<double>();
+            for (double i = 0; i < End; i += (Math.PI))
+            {
+                asymptotes.Add(((1 / K) * i) - D);
+            }
+            for (double i = 0; i > Start; i -= (Math.PI))
+            {
+                asymptotes.Add(((1 / K) * i) - D);
+            }
+            return asymptotes;
+        }
+    }
+
+    class CotFunction : PlotFunction
+    {
+        public CotFunction(PlotCanvas canvas) : base(canvas)
+        {
+        }
+
+        protected override double Parent(double x)
+        {
+            return 1 / Math.Tan(x);
+        }
+
+        protected override List<double> Asymptotes()
+        {
+            List<double> asymptotes = new List<double>();
+            for (double i = 0; i < End; i += (Math.PI))
+            {
+                asymptotes.Add(((1 / K) * i) - D);
+            }
+            for (double i = 0; i > Start; i -= (Math.PI))
+            {
+                asymptotes.Add(((1 / K) * i) - D);
+            }
+            return asymptotes;
+        }
+    }
+
+    class QuadraticFunction : PlotFunction
+    {
+        public QuadraticFunction(PlotCanvas canvas) : base(canvas)
+        {
+        }
+
+        protected override double Parent(double x)
+        {
+            return Math.Pow(x, 2);
+        }
+    }
+
+    class SquareRootFunction : PlotFunction
+    {
+        public SquareRootFunction(PlotCanvas canvas) : base(canvas)
+        {
+        }
+
+        protected override double Parent(double x)
+        {
+            return Math.Sqrt(x);
+        }
+    }
+
+    class CubicFunction : PlotFunction
+    {
+        public CubicFunction(PlotCanvas canvas) : base(canvas)
+        {
+        }
+
+        protected override double Parent(double x)
+        {
+            return Math.Pow(x, 3);
+        }
+    }
+
+    class RationalFunction : PlotFunction
+    {
+        public RationalFunction(PlotCanvas canvas) : base(canvas)
+        {
+        }
+
+        protected override double Parent(double x)
+        {
+            return 1 / Math.Pow(x, 2);
+        }
+
+        protected override List<double> Asymptotes()
+        {
+            return new List<double> { -D };
         }
     }
 
