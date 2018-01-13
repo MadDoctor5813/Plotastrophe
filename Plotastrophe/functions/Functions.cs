656c4642b17809f7cdc25f24b2a07c550f567177
Kevin <dragn194@gmail.com>
2018-01-12 19:56:04 -0500

    Move all derived functions into one file

---
+++ Plotastrophe/functions/Functions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Plotastrophe.functions
+{
+    class LinearFunction : PlotFunction
+    {
+        public LinearFunction(PlotCanvas canvas) : base(canvas)
+        {
+        }
+
+        protected override double Parent(double x)
+        {
+            return x;
+        }
+    }
+
+    class QuarticFunction : PlotFunction
+    {
+        public QuarticFunction(PlotCanvas canvas) : base(canvas)
+        {
+        }
+
+        protected override double Parent(double x)
+        {
+            return Math.Pow(x, 4);
+        }
+    }
+
+    class QuinticFunction : PlotFunction
+    {
+        public QuinticFunction(PlotCanvas canvas) : base(canvas)
+        {
+        }
+
+        protected override double Parent(double x)
+        {
+            return Math.Pow(x, 5);
+        }
+    }
+
+    class RationalFunction : PlotFunction
+    {
+        public RationalFunction(PlotCanvas canvas) : base(canvas)
+        {
+        }
+
+        protected override double Parent(double x)
+        {
+            return 1 / x;
+        }
+    }
+
+}
